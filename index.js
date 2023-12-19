37bd9f75fad1ac83d2557f5a19a7779e89702aa2
Priyanshushekhar77 <way6shekhar@gmail.com>
2023-12-19 10:19:58 +0530

    express

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const express=require("express");
 const app = express();
 
+const dupdata = {
+    "name":"prince",
+    "rooll":"2021ug2008",
+    "id":15
+}
 
 app.get('/', (req,res) => {
      res.send('defining homepage')
@@ -10,6 +15,32 @@ app.get('/', (req,res) => {
 
 app.get('/apnapage', (req,res) => {
     res.send('<h1>apna wala page open hoga</h1>')
+})
+
+app.use('/apna',(req, res, next) => {
+    // console.log('middleware use ho rha hai')
+    
+    console.log('Time:', Date.now())
+    next()
+    
+  })
+
+app.get('/aboutyou', (req,res) => {
+    // res.json( {jsonformat: 'something blew up'})
+    // res.status(500).json({ error: 'message' })
+    res.links({
+        next: 'https://github.com/Priyanshushekhar77/backendpractice'
+        // last: 'http://api.example.com/users?page=5'
+      })
+})
+
+app.get('/github',(req,res)=>{
+    res.json(dupdata)
+
+})
+
+app.get('/sendpath',(req,res) => {
+    res.status(404).sendFile('C:\Users\Priyanshu\Desktop\project\teaching\image.jpg')
 })
 
 //console me ayega
